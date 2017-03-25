34a855abf4a2ab6c66fdf998f37c156b5b74a816
jeffreyfriedman <jeff@eurodollaryen.com>
2017-03-24 22:12:40 -0400

    Restore users api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  # devise_for :users, defaults: { format: :json },
-  # controllers: {
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations',
-  #   omniauth_callbacks: 'users/omniauth_callbacks'
-  # }
-  #
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :users, only: [:show]
-  #   end
-  # end
+  devise_for :users, defaults: { format: :json },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show]
+    end
+  end
 end
