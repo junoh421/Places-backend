76780447b189e335a8f522fb0fd17f494fd30805
jeffreyfriedman <jeff@eurodollaryen.com>
2017-03-26 12:04:52 -0400

    Omniauth modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
   devise_for :users, defaults: { format: :json },
   controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  } do
+    authenticated :user do
+      delete 'sign_out', :to => 'devise/sessions#destroy'
+      root "projects#index", as: :authenticated_root
+      resources :users
+    end
+    unauthenticated :user do
+      match '/sessions/user', to: 'devise/sessions#create', via: :post
+    end
+  end
 
   namespace :api do
     namespace :v1 do
